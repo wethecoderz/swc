b14c6cd0ca8a9d1c60aed11e73eb37b5b9971268
wethecoderz <88876166+wethecoderz@users.noreply.github.com>
2022-06-05 08:30:35 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <div className="card text-center">
+                <div className="card-header">
+                    Featured
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Sarkari Word Counter</h5>
+                    <p className="card-text">We count, because you don't need to count manually.</p>
+                    <a href="/" className="btn btn-success">Go somewhere</a>
+                </div>
+                <div className="card-footer text-muted">
+                    via ~ Akashp2810
+                </div>
+            </div>
+        </div>
+    )
+}
